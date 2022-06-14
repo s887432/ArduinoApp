96d6130e8e867ae4625c9c49d50f807d5dd9fa3a
Patrick Lin <patrick.cclin@gmail.com>
2022-06-14 21:31:00 +0800

    Add files via upload

---
+++ ServoTest_01.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+int PIN_SERVO_1 = 9;
+Servo gServo;
+int pos;
+int dir;
+
+void setup() {
+  Serial.begin(115200);
+  gServo.attach(PIN_SERVO_1);
+  pos = 0;
+  dir = 1;
+}
+
+void loop() {
+  Serial.print("Moving to : ");
+  Serial.println(pos, DEC);
+  
+  gServo.write(pos);
+  delay(300);
+  
+  if( pos >= 180 ) {
+    dir = -1;
+  }
+  else if ( pos <= 0 ){
+    dir = 1;
+  }
+
+  pos = pos + dir * 10;
+}
